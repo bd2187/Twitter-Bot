e6ac0f12a21b2a0b8d0a57cc4877e9c1f88456a7
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-05-28 13:38:06 -0700

    Add createTweet function to post tweet.

--- app.js
+++ app.js
@@ -2,3 +2,17 @@ var Twit = require('twit');
 var config = require('./config');
 
 var T = new Twit(config);
+
+function createTweet() {
+  var tweet = { status: 'Testing...' };
+
+  function tweetData(err, data, response) {
+    return err
+    ? console.log('Something went wrong', err)
+    : console.log('Success!', data.text);
+  }
+
+  T.post('statuses/update', tweet, tweetData);
+}
+
+createTweet();
